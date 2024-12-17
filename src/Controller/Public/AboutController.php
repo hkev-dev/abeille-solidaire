174fc9f82b104a62e877617f199db58875f06b9a
dahromy <romeorazaf@gmail.com>
2024-12-17 22:28:05 +0300

    Adds About page with team and testimonials sections
    Implements complete About page functionality including:
    - Team member showcase with social links
    - Customer testimonials carousel
    - Brand partnerships section
    - Progress indicators and company intro
    - Call-to-action sections
    
    Organizes components into reusable Twig templates for better maintainability

---
+++ src/Controller/Public/AboutController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Controller\Public;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AboutController extends AbstractController
+{
+    #[Route('/about', name: 'landing.about')]
+    public function index(): Response
+    {
+        $testimonials = [
+            [
+                'name' => 'Sarah Albert',
+                'position' => 'CO Founder',
+                'image' => 'testimonial-two-client-img-1.jpg',
+                'text' => 'Exercitation ullamco laboris nisi ut aliquip ex ea ex commodo consequat duis aute aboris nisi ut aliquip irure reprehederit in voluptate velit esse.',
+                'rating' => 5
+            ],
+            [
+                'name' => 'Kevin Martin',
+                'position' => 'CO Founder',
+                'image' => 'testimonial-two-client-img-2.jpg',
+                'text' => 'Exercitation ullamco laboris nisi ut aliquip ex ea ex commodo consequat duis aute aboris nisi ut aliquip irure reprehederit in voluptate velit esse.',
+                'rating' => 5
+            ],
+            [
+                'name' => 'Kevin Coper',
+                'position' => 'CO Founder',
+                'image' => 'testimonial-two-client-img-3.jpg',
+                'text' => 'Exercitation ullamco laboris nisi ut aliquip ex ea ex commodo consequat duis aute aboris nisi ut aliquip irure reprehederit in voluptate velit esse.',
+                'rating' => 5
+            ],
+            [
+                'name' => 'Jessica Brown',
+                'position' => 'CO Founder',
+                'image' => 'testimonial-two-client-img-4.jpg',
+                'text' => 'Exercitation ullamco laboris nisi ut aliquip ex ea ex commodo consequat duis aute aboris nisi ut aliquip irure reprehederit in voluptate velit esse.',
+                'rating' => 5
+            ]
+        ];
+
+        $teamMembers = [
+            [
+                'name' => 'Mike Hardson',
+                'position' => 'Consultant',
+                'image' => 'team-1-1.jpg',
+                'social' => [
+                    'twitter' => '#',
+                    'facebook' => '#',
+                    'instagram' => '#'
+                ]
+            ],
+            [
+                'name' => 'Sarah Albert',
+                'position' => 'Manager',
+                'image' => 'team-1-2.jpg',
+                'social' => [
+                    'twitter' => '#',
+                    'facebook' => '#',
+                    'instagram' => '#'
+                ]
+            ],
+            [
+                'name' => 'Kevin Martin',
+                'position' => 'Director',
+                'image' => 'team-1-3.jpg',
+                'social' => [
+                    'twitter' => '#',
+                    'facebook' => '#',
+                    'instagram' => '#'
+                ]
+            ]
+        ];
+
+        return $this->render('public/pages/about/index.html.twig', [
+            'testimonials' => $testimonials,
+            'team_members' => $teamMembers
+        ]);
+    }
+}
