e9fd6c875df3b6551c53082fdfd77f8db886138e
Andrianirija Lafatra <andrianirijanyavo@gmail.com>
2025-02-09 00:18:36 +0300

    features - admin - withdrawal init

---
+++ src/Controller/Admin/WithdrawalController.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Admin;
+
+use App\Repository\DonationRepository;
+use App\Repository\ProjectRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route('/admin/withdrawal', name: 'app.admin.withdrawal.')]
+#[IsGranted('ROLE_ADMIN')]
+class WithdrawalController extends AbstractController
+{
+    #[Route('/request', name: 'request')]
+    public function request(Request $request, ProjectRepository $projectRepository, PaginatorInterface $paginator): Response
+    {
+        $query = $projectRepository->createQueryBuilder('project')
+            ->orderBy('project.updatedAt', 'DESC');
+
+        $pagination = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            10
+        );
+
+        return $this->render('admin/pages/withdrawal/request.html.twig', [
+            'pagination' => $pagination
+        ]);
+    }
+    #[Route('/charge', name: 'charge')]
+    public function charge(Request $request, ProjectRepository $projectRepository, PaginatorInterface $paginator): Response
+    {
+        $query = $projectRepository->createQueryBuilder('project')
+            ->orderBy('project.updatedAt', 'DESC');
+
+        $pagination = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            10
+        );
+
+        return $this->render('admin/pages/withdrawal/charge.html.twig', [
+            'pagination' => $pagination
+        ]);
+    }
+}
